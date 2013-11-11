fe04dcc8eaa7cac73300860177a9a4566cd2f4d3
Ryan <mryan23@vt.edu>
2013-11-10 21:15:21 -0500

    Skeleton working

--- UDPClient.java
+++ UDPClient.java
@@ -7,7 +7,7 @@ public class UDPClient{
 			DatagramSocket clientSocket=new DatagramSocket();
 			byte[] sendData = new byte[1024];
 			sendData=args[0].getBytes();
-			InetAddress host = InetAddress.getByName("192.168.1.7");
+			InetAddress host = InetAddress.getByName("24.219.213.60");
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,host, 6789);
 			clientSocket.send(sendPacket);
 			System.out.println("SENT");
