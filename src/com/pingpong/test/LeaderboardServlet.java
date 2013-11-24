885df38ed1ef512a37c3b9e7b4fcb7a60a9c1e5d
Callie Johnston <calliej7@vt.edu>
2013-11-24 16:44:30 -0500

    Add leaderboard

---
+++ src/com/pingpong/test/LeaderboardServlet.java
@@ -0,0 +1,58 @@
+package com.pingpong.test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class LeaderboardServlet extends HttpServlet {
+	
+	static Gson gson = new Gson();
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		Vector<String> scores = getUserScores();
+		
+		Response response = new Response();
+		response.result = scores;
+		
+		resp.setContentType("application/json");
+		resp.getWriter().write(gson.toJson(response));
+	}
+
+	public static Vector<String> getUserScores() {
+		Vector<String> resultVector = new Vector<String>();
+		
+		Connection c = PingPongWebServer.database_;
+		
+		PreparedStatement stmt;
+		try {
+			stmt = c.prepareStatement("SELECT * FROM class ORDER BY score DESC;");
+			ResultSet result = stmt.executeQuery();
+			while (result.next()) {
+				String name = result.getString("username");
+				int score = result.getInt("score");
+				resultVector.add(name + " " + String.valueOf(score));
+			}
+			result.close();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return resultVector;
+	}
+	
+	private class Response {
+		Vector<String> result;
+	}
+}
--- src/com/pingpong/test/PingPongWebServer.java
+++ src/com/pingpong/test/PingPongWebServer.java
@@ -2,6 +2,7 @@ package com.pingpong.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -58,5 +59,52 @@ public class PingPongWebServer extends HttpServlet {
 				+ "score INT NOT NULL)";
 		stmt.executeUpdate(sql);
 		stmt.close();
+	}
+	
+	public static void updateScores(String player1, String player2, int p1Score, int p2Score) {
+		int p1New = 0;
+		int p2New = 0;
+		
+		try {
+			PreparedStatement stmt = database_.prepareStatement("SELECT * FROM class WHERE username=?");
+			stmt.setString(1, player1);
+			ResultSet result = stmt.executeQuery();
+			while (result.next()) {
+				String name = result.getString("username");
+				if (player1.matches(name)) {
+					p1New = result.getInt("score");
+				}
+			}
+			result.close();
+			stmt.close();
+			
+			stmt = database_.prepareStatement("UPDATE class SET score=? WHERE username=?");
+			stmt.setInt(1, p1New + p1Score);
+			stmt.setString(2, player1);
+			stmt.executeUpdate();
+			stmt.close();
+			
+			stmt = database_.prepareStatement("SELECT * FROM class WHERE username=?");
+			stmt.setString(1, player2);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				String name = rs.getString("username");
+				if (player2.matches(name)) {
+					p2New = rs.getInt("score");
+				}
+			}
+			result.close();
+			stmt.close();
+			
+			stmt = database_.prepareStatement("UPDATE class SET score=? WHERE username=?");
+			stmt.setInt(1, p2New + p2Score);
+			stmt.setString(2, player2);
+			stmt.executeUpdate();
+			stmt.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
