9054833bfafa806eea05650ef902b29757d9ada4
mryan23 <mryan23@vt.edu>
2013-12-02 23:33:08 -0500

    Delete updateServer.sh

--- updateServer.sh
+++
@@ -1,2 +0,0 @@
-scp -r -i ~/.ssh/keypair_1.pem war/ ubuntu@ec2-184-72-139-63.compute-1.amazonaws.com:~/.
-scp -i ~/.ssh/keypair_1.pem pingpongserver.jar ubuntu@ec2-184-72-139-63.compute-1.amazonaws.com:~/.
